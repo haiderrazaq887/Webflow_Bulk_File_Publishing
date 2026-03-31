98027aaa65620eca5fdc1cdd70c2c90de0bd0360
haiderrazaq887 <haiderrazaq887@gmail.com>
2026-03-31 18:10:17 +0500

    Update create_publish.js

--- create_publish.js
+++ create_publish.js
@@ -137,6 +137,7 @@ app.get('/', (req, res) => {
     `);
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
